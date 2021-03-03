0429a8f588f93fbf4aa5fea81b022f82314bfdf7
Moises Camacho <moisescamacho26@gmail.com>
2021-03-03 19:01:12 -0400

    Creando la primera tarjeta del index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,26 @@ import Layout from "../component/layout";
 
 const Index = ()=> (
     <Layout>
-        <h1> hello word</h1>
+        {/** Header card */}
+        <header className="row">
+            <div className="col-md-12">
+                <div className="card card-body bg-secondary text-light">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <img src="/profile.jpg" alt="profile pic" className="img-fluid"/>
+                        </div>
+                        <div className="col-md-8">
+                            <h1> Elmet Astrudi </h1>
+                            <h3>Developer</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus ut tenetur architecto inventore voluptas quidem iure sequi suscipit, ipsam vel aut, voluptatum ducimus rem. Blanditiis et placeat maiores expedita. At!</p>
+                            <a href="/hire">Contratame</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
     </Layout>
 )
  export default Index;

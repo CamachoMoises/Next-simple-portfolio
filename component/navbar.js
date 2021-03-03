d4fa3b771f4acc7dd555f459b4da380074da334b
Moises Camacho <moisescamacho26@gmail.com>
2021-03-03 17:33:35 -0400

    Errutando con componente link

--- component/navbar.js
+++ component/navbar.js
@@ -1,23 +1,25 @@
+import Link from "next/link";
+
 const Navbar = ()=> (
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container">
-    <a className="navbar-brand" href="#">Simple Porfolio</a>
+      <Link href="/">
+        <a className="navbar-brand">Simple Porfolio</a>
+      </Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-        <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+            <Link href="/blog" >
+            <a className="nav-link" >Blog</a>
+            </Link>
         </li>
         <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <Link href="/github">
+            <a className="nav-link">GitHub</a>
+            </Link>
         </li>
         </ul>
     </div>
